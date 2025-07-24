0ee2fad88006a698fdba2632749527a54100440f
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-07-24 16:05:53 -0300

    checkulps: Fix float_ranges_t<double> for completeness

--- src/checkulps/checkulps.cc
+++ src/checkulps/checkulps.cc
@@ -67,17 +67,16 @@ template <> struct float_ranges_t<float>
   }
 };
 
-// TODO set correct values
 template <> struct float_ranges_t<double>
 {
-  static constexpr uint64_t plus_normal_min = 0x00800000;
-  static constexpr uint64_t plus_normal_max = 0x7F7FFFFF;
-  static constexpr uint64_t plus_subnormal_min = 0x00000001;
-  static constexpr uint64_t plus_subnormal_max = 0x007FFFFF;
-  static constexpr uint64_t neg_normal_min = 0x80800000;
-  static constexpr uint64_t neg_normal_max = 0xFF7FFFFF;
-  static constexpr uint64_t neg_subnormal_min = 0x80000001;
-  static constexpr uint64_t neg_subnormal_max = 0x807FFFFF;
+  static constexpr uint64_t plus_normal_min = UINT64_C(0x0008000000000000);
+  static constexpr uint64_t plus_normal_max = UINT64_C(0x7FEFFFFFFFFFFFFF);
+  static constexpr uint64_t plus_subnormal_min = UINT64_C(0x0000000000000001);
+  static constexpr uint64_t plus_subnormal_max = UINT64_C(0x000FFFFFFFFFFFFF);
+  static constexpr uint64_t neg_normal_min = UINT64_C(0x8008000000000000);
+  static constexpr uint64_t neg_normal_max = UINT64_C(0xFFEFFFFFFFFFFFFF);
+  static constexpr uint64_t neg_subnormal_min = UINT64_C(0x8000000000000001);
+  static constexpr uint64_t neg_subnormal_max = UINT64_C(0x800FFFFFFFFFFFFF);
 
   static constexpr double
   from_integer (uint64_t u)
