9747ee2dbce5c56dc2021fdc28af926090033e36
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-07-27 16:34:27 -0300

    checkulps: Remove lingering {uni,bi}variate definitions

--- src/checkulps/refimpls.cc
+++ src/checkulps/refimpls.cc
@@ -43,74 +43,74 @@ setup_ref_impl<double> ()
 
 extern "C"
 {
-#define _DEF_UNIVARIATE(__name)                                               \
+#define _DEF_F(__name)                                               \
   extern float ref_##__name##f (float, mpfr_rnd_t);                           \
   extern double ref_##__name (double, mpfr_rnd_t)
 
-#define DEF_UNIVARIATE(__name) _DEF_UNIVARIATE (__name)
+#define DEF_F(__name) _DEF_F (__name)
 
-#define DEF_UNIVARIATE_WEAK(__name)                                           \
+#define DEF_F_WEAK(__name)                                           \
   extern float __name##f (float) __attribute__ ((weak));                      \
   extern double __name (double) __attribute__ ((weak));                       \
-  _DEF_UNIVARIATE (__name)
+  _DEF_F (__name)
 
-#define _DEF_BIVARIATE(__name)                                                \
+#define _DEF_F_F(__name)                                                \
   extern float ref_##__name##f (float, float, mpfr_rnd_t);                    \
   extern double ref_##__name (double, double, mpfr_rnd_t)
 
-#define DEF_BIVARIATE(__name) _DEF_BIVARIATE (__name)
+#define DEF_F_F(__name) _DEF_F_F (__name)
 
-#define DEF_BIVARIATE_WEAK(__name)                                            \
+#define DEF_F_F_WEAK(__name)                                            \
   extern float __name##f (float, float) __attribute__ ((weak, used));         \
   extern double __name (double, double) __attribute__ ((weak, used));         \
-  _DEF_BIVARIATE (__name)
+  _DEF_F_F (__name)
 
-  DEF_BIVARIATE (atan2);
-  DEF_UNIVARIATE_WEAK (atanpi);
-  DEF_UNIVARIATE (acos);
-  DEF_UNIVARIATE (acosh);
-  DEF_UNIVARIATE_WEAK (acospi);
-  DEF_UNIVARIATE (asin);
-  DEF_UNIVARIATE (asinh);
-  DEF_UNIVARIATE_WEAK (asinpi);
-  DEF_UNIVARIATE (atan);
-  DEF_UNIVARIATE (atanh);
-  DEF_UNIVARIATE (cbrt);
-  DEF_UNIVARIATE (cos);
-  DEF_UNIVARIATE (cosh);
-  DEF_UNIVARIATE_WEAK (cospi);
-  DEF_UNIVARIATE (erf);
-  DEF_UNIVARIATE (erfc);
-  DEF_UNIVARIATE (exp);
-  DEF_UNIVARIATE (expm1);
-  DEF_UNIVARIATE_WEAK (exp10);
-  DEF_UNIVARIATE_WEAK (exp10m1);
-  DEF_UNIVARIATE (exp2);
-  DEF_UNIVARIATE_WEAK (exp2m1);
-  DEF_UNIVARIATE (lgamma);
-  DEF_UNIVARIATE (log);
-  DEF_UNIVARIATE (log1p);
-  DEF_UNIVARIATE (log2);
-  DEF_UNIVARIATE_WEAK (log2p1);
-  DEF_UNIVARIATE (log10);
-  DEF_UNIVARIATE_WEAK (log10p1);
-  DEF_BIVARIATE (hypot);
-  DEF_BIVARIATE (pow);
-  DEF_UNIVARIATE_WEAK (rsqrt);
-  DEF_UNIVARIATE (sin);
-  DEF_UNIVARIATE (sinh);
-  DEF_UNIVARIATE_WEAK (sinpi);
-  DEF_UNIVARIATE (tan);
-  DEF_UNIVARIATE (tanh);
-  DEF_UNIVARIATE_WEAK (tanpi);
-  DEF_UNIVARIATE (tgamma);
+  DEF_F_F (atan2);
+  DEF_F_WEAK (atanpi);
+  DEF_F (acos);
+  DEF_F (acosh);
+  DEF_F_WEAK (acospi);
+  DEF_F (asin);
+  DEF_F (asinh);
+  DEF_F_WEAK (asinpi);
+  DEF_F (atan);
+  DEF_F (atanh);
+  DEF_F (cbrt);
+  DEF_F (cos);
+  DEF_F (cosh);
+  DEF_F_WEAK (cospi);
+  DEF_F (erf);
+  DEF_F (erfc);
+  DEF_F (exp);
+  DEF_F (expm1);
+  DEF_F_WEAK (exp10);
+  DEF_F_WEAK (exp10m1);
+  DEF_F (exp2);
+  DEF_F_WEAK (exp2m1);
+  DEF_F (lgamma);
+  DEF_F (log);
+  DEF_F (log1p);
+  DEF_F (log2);
+  DEF_F_WEAK (log2p1);
+  DEF_F (log10);
+  DEF_F_WEAK (log10p1);
+  DEF_F_F (hypot);
+  DEF_F_F (pow);
+  DEF_F_WEAK (rsqrt);
+  DEF_F (sin);
+  DEF_F (sinh);
+  DEF_F_WEAK (sinpi);
+  DEF_F (tan);
+  DEF_F (tanh);
+  DEF_F_WEAK (tanpi);
+  DEF_F (tgamma);
 
-#undef _DEF_UNIVARIATE
-#undef DEF_UNIVARIATE
-#undef DEF_UNIVARIATE_WEAK
-#undef _DEF_BIVARIATE
-#undef DEF_BIVARIATE
-#undef DEF_BIVARIATE_WEAK
+#undef _DEF_F
+#undef DEF_F
+#undef DEF_F_WEAK
+#undef _DEF_F_F
+#undef DEF_F_F
+#undef DEF_F_F_WEAK
 };
 
 template <typename F, typename F_MPFR> struct func_f_desc_t
