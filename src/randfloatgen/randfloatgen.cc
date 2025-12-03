177bfc539b0e4d6a02925fe22d3e08b41b602a51
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-12-03 10:44:41 -0300

    ranfloatgen: Add support for specify special values

--- src/randfloatgen/randfloatgen.cc
+++ src/randfloatgen/randfloatgen.cc
@@ -17,6 +17,7 @@
 
 #include "floatranges.h"
 #include "iohelper.h"
+#include "strhelper.h"
 #include "wyhash64.h"
 
 using namespace iohelper;
@@ -107,11 +108,8 @@ gen_f_f (const std::optional<std::string> &nameopt,
 template <typename F>
 static void
 gen_f_f_f (const std::optional<std::string> &nameopt,
-	   const std::optional<std::string> &argsopt,
-	   F fstart0, F fend0,
-	   F fstart1, F fend1,
-	   F fstart2, F fend2,
-	   int count)
+	   const std::optional<std::string> &argsopt, F fstart0, F fend0,
+	   F fstart1, F fend1, F fstart2, F fend2, int count)
 {
   std::string name;
   if (nameopt.has_value ())
@@ -145,9 +143,8 @@ gen_f_f_f (const std::optional<std::string> &nameopt,
       bool isnegative0 = f0 < 0.0;
       bool isnegative1 = f1 < 0.0;
       bool isnegative2 = f2 < 0.0;
-      std::println ("{}0x{:a}, {}0x{:a}, {}0x{:a}",
-		    isnegative0 ? "-" : "", std::fabs (f0),
-		    isnegative1 ? "-" : "", std::fabs (f1),
+      std::println ("{}0x{:a}, {}0x{:a}, {}0x{:a}", isnegative0 ? "-" : "",
+		    std::fabs (f0), isnegative1 ? "-" : "", std::fabs (f1),
 		    isnegative2 ? "-" : "", std::fabs (f2));
     }
 }
@@ -184,8 +181,7 @@ handle_f_f (const std::optional<std::string> &nameopt,
 static void
 handle_f_f_f (const std::optional<std::string> &nameopt,
 	      const std::optional<std::string> &argsopt,
-	      const std::string &type,
-	      int count,
+	      const std::string &type, int count,
 	      const std::vector<double> &ranges_x,
 	      const std::vector<double> &ranges_y,
 	      const std::vector<double> &ranges_z)
@@ -195,7 +191,7 @@ handle_f_f_f (const std::optional<std::string> &nameopt,
 		      ranges_y[1], ranges_z[0], ranges_z[1], count);
   else if (type == "binary64")
     gen_f_f_f<double> (nameopt, argsopt, ranges_x[0], ranges_x[1], ranges_y[0],
-		     ranges_y[1], ranges_z[0], ranges_z[1],count);
+		       ranges_y[1], ranges_z[0], ranges_z[1], count);
   else
     error ("invalid type {}", type);
 }
@@ -205,6 +201,78 @@ error (const std::string &str)
 {
   std::cerr << "error: " << str << '\n';
   std::exit (EXIT_FAILURE);
+}
+
+static inline std::string
+adjustSignal (const std::string &s)
+{
+  return s.starts_with ("\\-") ? "-" + s.substr (2) : s;
+}
+
+template <typename F>
+static const std::vector<F>
+rangeStrToFloat (const std::vector<std::string> &values)
+{
+  auto floatview = values | std::views::transform ([] (const std::string &s) {
+		     auto trimmed = adjustSignal (std::string(strhelper::trim (s)));
+		     if (trimmed == "-pi")
+		       return -std::numbers::pi_v<F>;
+		     else if (trimmed == "pi")
+		       return std::numbers::pi_v<F>;
+		     else if (trimmed == "2pi")
+		       return F(2.0) * std::numbers::pi_v<F>;
+		     else if (trimmed == "min")
+		       return std::numeric_limits<F>::min ();
+		     else if (trimmed == "-min")
+		       return -std::numeric_limits<F>::min ();
+		     else if (trimmed == "max")
+		       return std::numeric_limits<F>::max ();
+		     else if (trimmed == "-max")
+		       return -std::numeric_limits<F>::max ();
+		     if (auto n = floatrange::fromStr<F> (s); n.has_value ())
+		       return n.value ();
+		     else
+		       error ("invalid number: {}", s);
+		   });
+
+  return std::vector<F> (floatview.begin (), floatview.end ());
+}
+
+template <typename F>
+static void
+handleType (const argparse::ArgumentParser &options,
+	    const std::optional<std::string> &name,
+	    const std::optional<std::string> &args, int count)
+{
+  auto range_x
+      = rangeStrToFloat<F> (options.get<std::vector<std::string> > ("-x"));
+  if (range_x[0] > range_x[1])
+    error ("invalid range definitions [{},{}]", range_x[0], range_x[1]);
+
+  if (!options.is_used ("-y"))
+    gen_f<F> (name, args, range_x[0], range_x[1], count);
+  else
+    {
+      auto range_y
+	  = rangeStrToFloat<F> (options.get<std::vector<std::string> > ("-y"));
+      if (range_y[0] > range_y[1])
+	error ("invalid range definitions [{},{}]", range_y[0], range_y[1]);
+
+      if (!options.is_used ("-z"))
+	gen_f_f<F> (name, args, range_x[0], range_x[1], range_y[0], range_y[1],
+		    count);
+      else
+	{
+	  auto range_z = rangeStrToFloat<F> (
+	      options.get<std::vector<std::string> > ("-z"));
+	  if (range_z[0] > range_z[1])
+	    error ("invalid range definitions [{},{}]", range_z[0],
+		   range_z[1]);
+
+	  gen_f_f_f<F> (name, args, range_x[0], range_x[1], range_y[0],
+			range_y[1], range_z[0], range_z[1], count);
+	}
+    }
 }
 
 int
@@ -218,27 +286,23 @@ main (int argc, char *argv[])
       .default_value ("binary32")
       .store_into (type);
 
-  // We always parse in binary64, even for binary32
   options.add_argument ("-x")
       .help ("range to use in the form '<start> <end>'")
       .required ()
-      .nargs (2)
-      .scan<'g', double> ();
+      .nargs (2);
 
   options.add_argument ("-y")
       .help ("range to use in the form '<start> <end>'")
-      .nargs (2)
-      .scan<'g', double> ();
+      .nargs (2);
 
   options.add_argument ("-z")
       .help ("range to use in the form '<start> <end>'")
-      .nargs (2)
-      .scan<'g', double> ();
+      .nargs (2);
 
   options.add_argument ("--count", "-c")
       .help (std::format ("numbers to generate (default {}", kDefaultCount))
       .default_value (kDefaultCount)
-      .scan<'i', int>();
+      .scan<'i', int> ();
 
   options.add_argument ("--name", "-n");
   options.add_argument ("--args", "-a");
@@ -252,7 +316,7 @@ main (int argc, char *argv[])
       error ("{}", err.what ());
     }
 
-  int count = options.get<int>("-c");
+  int count = options.get<int> ("-c");
 
   std::optional<std::string> name;
   if (options.is_used ("--name"))
@@ -261,27 +325,10 @@ main (int argc, char *argv[])
   if (options.is_used ("--args"))
     name = options.get<std::string> ("--args");
 
-  auto range_x = options.get<std::vector<double> > ("-x");
-  if (range_x[0] > range_x[1])
-    error ("invalid range definitions [{},{}]", range_x[0], range_x[1]);
-
-  if (!options.is_used ("-y"))
-    handle_f (name, args, type, count, range_x);
+  if (type == "binary32")
+    handleType<float> (options, name, args, count);
+  else if (type == "binary64")
+    handleType<double> (options, name, args, count);
   else
-    {
-      auto range_y = options.get<std::vector<double> > ("-y");
-      if (range_y[0] > range_y[1])
-	error ("invalid range definitions [{},{}]", range_y[0], range_y[1]);
-
-      if (!options.is_used ("-z"))
-	handle_f_f (name, args, type, count, range_x, range_y);
-      else
-	{
-	  auto range_z = options.get<std::vector<double> >("-z");
-	  if (range_z[0] > range_z[1])
-	    error ("invalid range definitions [{},{}]", range_z[0], range_z[1]);
-
-	  handle_f_f_f (name, args, type, count, range_x, range_y, range_z);
-	}
-    }
+    error ("invalid type{}", type);
 }
